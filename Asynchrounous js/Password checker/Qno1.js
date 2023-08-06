078d5125bb40d431082c46d17f61bc16ce8f8264
Qasim <qasimyousaf094@gmail.com>
2023-08-07 01:56:13 +0500

    first commit

--- Asynchrounous js/Password checker/Qno1.js
+++ Asynchrounous js/Password checker/Qno1.js
@@ -1,2 +1,11 @@
-ssssssssssssssssssssssssss
+/* 1. Set up a Promise that resolves with a value of Start Counting . 2. Create a function named counter() that has one argument that gets
+the value and outputs it into the console.
+
+3. Set up the next function in the Promise with four then() instances,
+which should output a value into the counter function, and return a
+value which will provide input for the subsequent then() instance.
+The returned values should be one , then two , then three . The
+screen output in the console should be the following:
+*/
+
 
